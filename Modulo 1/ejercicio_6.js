5665094a917e50ca556756200cf978f98c7c3699
Carlos Gomez <carlosfgr00s@gmail.com>
2024-05-08 21:06:05 -0600

    ejercicio 6 modulo 1

---
+++ Modulo 1/ejercicio_6.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const rl = readline.createInterface
+({
+    input : process.stdin,
+    output : process.stdout
+});
+
+rl.question("Ingrese una frase: ", (frase) =>{
+    const palabras = frase.split(' ');
+    console.log("La frase tiene " + palabras.length + " palabras");
+    rl.close();
+})
+
