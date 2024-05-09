9a776fc441b51e8fb3ef8c2e40a823b17e18d5c6
Carlos Gomez <carlosfgr00s@gmail.com>
2024-05-09 15:22:18 -0600

    nueva estructura de carpetas y ejercicios sesion 2

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let Persona = 
+{
+    nombre: "Carlos",
+    apellido: "Gomez",
+    edad: 23
+};
+
+console.log(`Hola ${Persona['nombre']} ${Persona['apellido']} tienes ${Persona['edad']} años.`);
