535821dfb63b7f2bf1b1d429c0472343526193d7
Carlos Gomez <carlosfgr00s@gmail.com>
2024-05-16 13:34:45 -0600

    ejercicio email utilizando nodemailer

---
+++ Modulo 1/sesion5/ejercicioEmail.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // Use `true` for port 465, `false` for all other ports
+  auth: {
+    user: "",
+    pass: ""
+  }
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: '', // sender address
+    to: "", // list of receivers
+    subject: "", // Subject line
+    text: "" // plain text body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+main().catch(console.error);
